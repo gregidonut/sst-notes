44f59cda2f5844bc38a236fe97a1168b1f1313f6
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-03-24 18:07:24 +0800

    feat: add clerk authentication to astro
    where all routes except for /sign-in is public

--- packages/frontend/astro.config.mjs
+++ packages/frontend/astro.config.mjs
@@ -4,12 +4,21 @@ import { defineConfig } from "astro/config";
 import compressor from "astro-compressor";
 import aws from "astro-sst";
 
+import clerk from "@clerk/astro";
+
+import { shadesOfPurple } from "@clerk/themes";
 // https://astro.build/config
 export default defineConfig({
-    integrations: [compressor()],
+    integrations: [
+        compressor(),
+        clerk({
+            appearance: {
+                baseTheme: shadesOfPurple,
+            },
+        }),
+    ],
     output: "server",
     adapter: aws({
         responseMode: "stream",
     }),
 });
-
