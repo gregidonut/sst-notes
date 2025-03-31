dc72e51139366749247f9a975eb26c4faea2fbc4
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-03-31 18:10:47 +0800

    test: before and after test a little more comprehensive
    folling more best practices for cypress as well hook into astro
    components with the data-cy attribute

--- cypress/e2e/crud.cy.ts
+++ cypress/e2e/crud.cy.ts
@@ -4,14 +4,23 @@ describe("crud", () => {
     cy.get("@clerkToken").then((token) => {
       cy.task("seedDyanamoDB", token);
     });
+    cy.visit("/notes");
+    cy.get('[data-cy="note-list"] > ol')
+      .as("noteList")
+      .children()
+      .should("have.length", 5);
   });
   afterEach(() => {
     cy.get("@clerkToken").then((token) => {
       cy.task("emptyDynamoDB", token);
     });
+    cy.visit("/notes");
+
+    cy.get("@noteList").should("not.exist");
+    cy.get('[data-cy="note-list"] > p').contains("no items yet");
   });
 
-  it("should find an initialized database", () => {
-    cy.visit("/notes");
+  it("should create note", () => {
+    cy.visit("/create");
   });
 });
