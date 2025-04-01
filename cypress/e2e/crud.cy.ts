44c98b49cd55ff00b492176c78b8d5d7e96ab672
Gregory Babajide Villaflor Osilaja <gregosilaja@outlook.com>
2025-04-01 17:13:50 +0800

    test: add a condition check whether the db is empty on before each
    this way we dont have to rely if the afterEach() block ran after each
    test
    
    we just conditionally check if the db is empty at the beforeEach() block
    then make a decision on whether to seed the db or empty the db before
    seeding yet again

--- cypress/e2e/crud.cy.ts
+++ cypress/e2e/crud.cy.ts
@@ -1,26 +1,58 @@
 describe("crud", () => {
+  let seedLength = 5;
   beforeEach(() => {
     cy.signInAsUser();
-    cy.get("@clerkToken").then((token) => {
-      cy.task("seedDyanamoDB", token);
-    });
     cy.visit("/notes");
+    cy.get('[data-cy="note-list"]')
+      .children()
+      .then((element) => {
+        return element[0].tagName === "OL" ? false : true;
+      })
+      .then((dbEmpty) => {
+        switch (dbEmpty) {
+          case true:
+            cy.log("db is empty");
+            cy.get("@clerkToken").then((token) => {
+              cy.task("seedDyanamoDB", token);
+            });
+            cy.visit("/notes");
+
+            cy.get('[data-cy="note-list"] > ol')
+              .as("noteList")
+              .children()
+              .should("have.length", seedLength);
+            break;
+          case false:
+            cy.log("db is not empty");
+            cy.get("@clerkToken").then((token) => {
+              cy.task("emptyDynamoDB", token);
+            });
+            cy.visit("/notes");
+            cy.get('[data-cy="note-list"] > p').contains("no items yet");
+
+            cy.get("@clerkToken").then((token) => {
+              cy.task("seedDyanamoDB", token);
+            });
+            cy.visit("/notes");
+
+            cy.get('[data-cy="note-list"] > ol')
+              .as("noteList")
+              .children()
+              .should("have.length", seedLength);
+            break;
+          default:
+        }
+      });
+  });
+  it("should create note", () => {
+    cy.visit("/create");
+    const content = "pukingina mo hayup ka";
+    cy.get('[data-cy="content-field"]').type(content);
+    cy.get('[data-cy="create-content-button"]').click();
+    cy.url().should("eq", "http://localhost:4321/notes");
     cy.get('[data-cy="note-list"] > ol')
       .as("noteList")
       .children()
-      .should("have.length", 5);
-  });
-  afterEach(() => {
-    cy.get("@clerkToken").then((token) => {
-      cy.task("emptyDynamoDB", token);
-    });
-    cy.visit("/notes");
-
-    cy.get("@noteList").should("not.exist");
-    cy.get('[data-cy="note-list"] > p').contains("no items yet");
-  });
-
-  it("should create note", () => {
-    cy.visit("/create");
+      .should("have.length", seedLength + 1);
   });
 });
